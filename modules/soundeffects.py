a46faa0b80ce1a277ccc740c9510106a330978f5
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-01-10 08:42:03 +0100

    Add soundeffects module

---
+++ modules/soundeffects.py
@@ -0,0 +1,29 @@
+def instantrimshot(phenny, input):
+    phenny.say('http://www.instantrimshot.com/')
+instantrimshot.commands = ['instantrimshot']
+
+
+def sadtrombone(phenny, input):
+    phenny.say('http://www.sadtrombone.com/')
+sadtrombone.commands = ['sadtrombone']
+
+
+def dramabutton(phenny, input):
+    phenny.say('http://www.dramabutton.com/')
+dramabutton.commands = ['dramabutton']
+
+
+def burn(phenny, input):
+    phenny.say('http://www.youtube.com/watch?v=BLmwmX-mni8')
+burn.commands = ['burn']
+
+
+def pet(phenny, input):
+    phenny.say('Purr, purr! :3')
+pet.commands = ['pet']
+
+
+def git(phenny, input):
+    phenny.say('# On branch master // ' +
+               'nothing to commit (working directory clean)')
+git.commands = ['git']
